b709b9bbb315b2eaa7b6b00a612cd054efe2bcbd
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-02-05 12:24:01 -0300

    Parameters and Exception Handler

---
+++ 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/MathController.java
@@ -0,0 +1,82 @@
+package br.com.erudio.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.converters.NumberConverter;
+import br.com.erudio.exceptions.UnsupportedMathOperationException;
+import br.com.erudio.math.SimpleMath;
+
+@RestController
+public class MathController {
+
+	private SimpleMath math = new SimpleMath();
+
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double sum(@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+		if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		}
+		return math.sum(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+	}
+
+	@RequestMapping(value = "/subtraction/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double subtraction(@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+		if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		}
+		return math.subtraction(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+	}
+
+	@RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double multiplication(@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+		if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		}
+		return math.multiplication(NumberConverter.convertToDouble(numberOne),
+				NumberConverter.convertToDouble(numberTwo));
+	}
+
+	@RequestMapping(value = "/division/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double division(@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+		if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		}
+
+		if (NumberConverter.convertToDouble(numberTwo) == 0.0) {
+			throw new UnsupportedMathOperationException("Unable to perform division by zero!");
+		}
+		return math.division(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+	}
+
+	@RequestMapping(value = "/average/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double average(@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+		if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		}
+
+		return math.average(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+	}
+
+	@RequestMapping(value = "/sqrt/{number}", method = RequestMethod.GET)
+	public Double sqrt(@PathVariable(value = "number") String number) throws Exception {
+
+		if (!NumberConverter.isNumeric(number)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		}
+		return math.sqrt(NumberConverter.convertToDouble(number));
+	}
+
+}
---
+++ 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/math/SimpleMath.java
@@ -0,0 +1,30 @@
+package br.com.erudio.math;
+
+import br.com.erudio.exceptions.UnsupportedMathOperationException;
+
+public class SimpleMath {
+
+	public Double sum(Double numberOne, Double numberTwo) {
+		return numberOne + numberTwo;
+	}
+
+	public Double subtraction(Double numberOne, Double numberTwo) {
+		return numberOne - numberTwo;
+	}
+
+	public Double multiplication(Double numberOne, Double numberTwo) {
+		return numberOne * numberTwo;
+	}
+
+	public Double division(Double numberOne, Double numberTwo) {
+		return numberOne / numberTwo;
+	}
+
+	public Double average(Double numberOne, Double numberTwo) {
+		return (numberOne + numberTwo) / 2;
+	}
+
+	public Double sqrt(Double number) {
+		return Math.sqrt(number);
+	}
+}
